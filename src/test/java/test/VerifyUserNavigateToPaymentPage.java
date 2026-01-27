3d11b0bf7f5fb2cefb8e0d9bafd0937347c791cb
Duong Do <binhduong190599@gmail.com>
2026-01-27 08:07:10 +0700

    Update Allure Report

--- src/test/java/test/VerifyUserNavigateToPaymentPage.java
+++ src/test/java/test/VerifyUserNavigateToPaymentPage.java
@@ -1,14 +1,19 @@
 package test;
 
+import io.qameta.allure.*;
+import listeners.AllureTestListener;
 import org.testng.annotations.Test;
+import org.testng.annotations.Listeners;
 import page.DashboardPage;
 import utils.DateTimeUtils;
 
 import java.lang.reflect.Method;
 
+@Listeners(AllureTestListener.class)
 public class VerifyUserNavigateToPaymentPage extends BaseTest {
 
-    @Test
+    @Description("This test case is created to verify that user can see error message after clicking on backwards")
+    @Test(description = "Verify User Able To Backwards")
     public void verifyUserAbleToBackwards(Method method) throws InterruptedException {
         DashboardPage dashboardPage = new DashboardPage(driver);
         dashboardPage.selectTripOption("One way");
