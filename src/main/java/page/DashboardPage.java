ae257ea078e390cdd6c194756112c00d3ee7362c
Duong Do <binhduong190599@gmail.com>
2026-01-28 15:24:33 +0700

    Update

--- src/main/java/page/DashboardPage.java
+++ src/main/java/page/DashboardPage.java
@@ -1,6 +1,5 @@
 package page;
 
-import io.qameta.allure.Allure;
 import io.qameta.allure.Step;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -10,55 +9,40 @@ import org.openqa.selenium.support.FindBy;
 
 public class DashboardPage extends BasePage {
 
-    // === Locator ===
-    @FindBy(xpath = "//*[@placeholder=\"Departure\"]")
+    @FindBy(xpath = "//*[@placeholder='Departure']")
     @CacheLookup
-    WebElement txtDeparture;
+    private WebElement txtDeparture;
 
-    // @FindBy(xpath = "//*[@placeholder=\"Departure\"]")
-    // @CacheLookup
-    // WebElement drpdwnDeparture;
-
-    @FindBy(xpath = "//*[@placeholder=\"Destination\"]")
+    @FindBy(xpath = "//*[@placeholder='Destination']")
     @CacheLookup
-    WebElement txtDestination;
+    private WebElement txtDestination;
 
-    @FindBy(xpath = "//*[@id=\"ticketops-seeker-button\"]")
+    @FindBy(xpath = "//*[@id='ticketops-seeker-button']")
     @CacheLookup
-    WebElement optTrip;
+    private WebElement optTrip;
 
-        @FindBy(xpath = "//input[@alt-placeholder=\"Departure date\"]")
+    @FindBy(xpath = "//input[@alt-placeholder='Departure date']")
     @CacheLookup
-    WebElement txtDepatureDate;
+    private WebElement txtDepartureDate;
 
-    // @FindBy(xpath = "//*[@placeholder=\"Departure\"]")
-    // @CacheLookup
-    // WebElement drpdwnTicketTrip;
-
-    @FindBy(xpath = "//button[@id=\"buttonSubmit1\"]")
+    @FindBy(xpath = "//button[@id='buttonSubmit1']")
     @CacheLookup
-    WebElement btnSearch;
+    private WebElement btnSearch;
 
-    // a[@title="Valencia (VLC)" and contains(@class, 'ui-state-active')]
-    String drpdwnLocation = "//a[@title='%s' and contains(@class, 'ui-state-active')]";
-
-    String optTripSelection = "//li[@class=\"ui-menu-item\"]//span[text()='%s']";
+    private String optTripSelection = "//li[@class='ui-menu-item']//span[text()='%s']";
 
     public DashboardPage(WebDriver driver) {
         super(driver);
     }
 
-    // === Action ===
-    @Step("Select departure {departure}")
-    public void selectDepature(String depature) {
-        keyword.sendText(txtDeparture, depature);
+    @Step("Select departure: {departure}")
+    public void selectDepature(String departure) {
+        keyword.sendText(txtDeparture, departure);
     }
 
-    @Step("Select destination {destination}")
+    @Step("Select destination: {destination}")
     public void selectDestination(String destination) {
         keyword.sendText(txtDestination, destination);
-        // keyword.click(driver.findElement(By.xpath(String.format(drpdwnLocation,
-        // destination))));
     }
 
     @Step("Click Search button")
@@ -66,21 +50,15 @@ public class DashboardPage extends BasePage {
         keyword.click(btnSearch);
     }
 
-    @Step("Select {option}")
+    @Step("Select trip option: {option}")
     public void selectTripOption(String option) {
         keyword.click(optTrip);
-        keyword.click(driver.findElement(By.xpath(String.format(optTripSelection, option))));
+        By optionLocator = By.xpath(String.format(optTripSelection, option));
+        keyword.click(driver.findElement(optionLocator));
     }
 
-    @Step("Set departure date {text}")
+    @Step("Set departure date: {text}")
     public void setTextDeparture(String text) {
-        Allure.step("set departure date");
-        keyword.sendText(txtDepatureDate, text);
+        keyword.sendText(txtDepartureDate, text);
     }
-
-    // === Assertion ===
-    // public boolean isDashboardPageDisplayed() {
-    // return lblOverview.isDisplayed();
-    // }
-
 }
